99eb34610bae333a958f776dd0269e25083c71e8
Quico Saval <quicosv@gmail.com>
2023-05-29 19:32:04 +0200

    Añade los puntos y coma y la función del IVA

--- backend/helpers/funciones.ts
+++ backend/helpers/funciones.ts
@@ -1,11 +1,15 @@
 const descuento = (importe: number) => {
 	if (importe > 1000) {
-		return importe*0.2
+		return importe*0.2;
 	} else {
-		return importe*0.1
-	}
-}
+		return importe*0.1;
+	};
+};
 
 const bruto = (importe: number, descuento: number) => {
 	return importe - descuento;
-}
+};
+
+const iva = (bruto: number) => {
+	return bruto * 0.21;
+};
