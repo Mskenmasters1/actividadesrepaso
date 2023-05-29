dc96bb590d8d42ef59d850e9a1037dce13a5e0af
Quico Saval <quicosv@gmail.com>
2023-05-29 22:20:29 +0200

    Añade la interface que da tipo a lo que devuelve el servidor

--- frontend/src/interfaces/importe.interface.ts
+++ frontend/src/interfaces/importe.interface.ts
@@ -1,3 +1,12 @@
+// Esto tipa lo que viene del formulario
 export interface IImporte {
 	importe: number;
-}
+};
+
+// Esto tipa lo que devuelve el servidor
+export interface IImporteResponse {
+	descuento: number;
+	bruto: number;
+	iva: number;
+	neto: number;
+};
