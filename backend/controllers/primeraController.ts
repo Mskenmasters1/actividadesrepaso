138f81a85e8f6916964ba221304bf882090f8c3b
Bilal <mskenmasters@hotmail.com>
2023-05-30 17:07:27 +0200

    crear archivo primeraController

---
+++ backend/controllers/primeraController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express';
+
+
+export const primeraGet = async (req: Request, res: Response) => {
+  
+};
+
+export const primeraPost = async (req: Request, res: Response) => {
+  
+};
