aff0a70042410abab1b135c5d5a303160911a949
Bilal <mskenmasters@hotmail.com>
2023-05-30 17:50:48 +0200

    añadir todas las rutas desde segunda asta cuarta y sus importaciones

--- backend/routes/routerActividades1.ts
+++ backend/routes/routerActividades1.ts
@@ -2,6 +2,9 @@ import { Router } from 'express';
 
 import { ejemploGet, ejemploPost } from '../controllers/actividades1Controller';
 import { primeraGet, primeraPost } from '../controllers/primeraController';
+import { segundaGet } from '../controllers/segundaController';
+import { terceraGet } from '../controllers/terceraController';
+import { cuartaGet } from '../controllers/cuartaController';
 
 export const routerActividades1 = Router();
 
@@ -9,3 +12,10 @@ routerActividades1.get('/ejemplo/:email/:password', ejemploGet);
 routerActividades1.post('/ejemplo', ejemploPost);
 
 routerActividades1.get('/primera/:importe', primeraGet);
+
+routerActividades1.get('/segunda/:palabra', segundaGet);
+
+routerActividades1.get('/tercera/:persona', terceraGet);
+routerActividades1.get('/tercera/:poblacion', terceraGet);
+
+routerActividades1.get('/cuarta/:aleatorio', cuartaGet);
