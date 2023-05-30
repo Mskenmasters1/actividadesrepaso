0709e37b98dcf443eb1097cbe5d3dda8672efa27
Quico Saval <quicosv@gmail.com>
2023-05-30 18:54:57 +0200

    Añade la palabra export en las definiciones de las funciones de los helpers

--- backend/helpers/funciones.ts
+++ backend/helpers/funciones.ts
@@ -1,4 +1,4 @@
-const descuento = (importe: number) => {
+export const descuento = (importe: number) => {
 	if (importe > 1000) {
 		return importe * 0.2;
 	} else {
@@ -6,19 +6,19 @@ const descuento = (importe: number) => {
 	}
 };
 
-const bruto = (importe: number, descuento: number) => {
+export const bruto = (importe: number, descuento: number) => {
 	return importe - descuento;
 };
 
-const iva = (bruto: number) => {
+export const iva = (bruto: number) => {
 	return bruto * 0.21;
 };
 
-const neto = (bruto: number, iva: number) => {
+export const neto = (bruto: number, iva: number) => {
 	return bruto + iva;
 };
 
-const invertirPalabra = (palabra: string) => {
+export const invertirPalabra = (palabra: string) => {
 	let palabraAlReves: string = "";
 	for (let i = palabra.length - 1; i >= 0; i--) {
 		palabraAlReves += i;
