88566c1484fda34f791eaed84ee542faaa5446aa
piscinagua <correo_javi@hotmail.com>
2023-05-30 18:51:01 +0200

    Crea función de invertirPalabra para la parte 2.

--- backend/helpers/funciones.ts
+++ backend/helpers/funciones.ts
@@ -1,9 +1,9 @@
 const descuento = (importe: number) => {
 	if (importe > 1000) {
-		return importe*0.2;
+		return importe * 0.2;
 	} else {
-		return importe*0.1;
-	};
+		return importe * 0.1;
+	}
 };
 
 const bruto = (importe: number, descuento: number) => {
@@ -13,6 +13,16 @@ const bruto = (importe: number, descuento: number) => {
 const iva = (bruto: number) => {
 	return bruto * 0.21;
 };
+
 const neto = (bruto: number, iva: number) => {
-	 return bruto + iva;
+	return bruto + iva;
 };
+
+const invertirPalabra = (palabra: string) => {
+	let palabraAlReves: string = "";
+	for (let i = palabra.length - 1; i >= 0; i--) {
+		palabraAlReves += i;
+	}
+	return palabraAlReves;
+};
+
