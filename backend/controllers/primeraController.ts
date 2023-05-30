873da11630d198c173bee360b9c9d398ba3ba264
Quico Saval <quicosv@gmail.com>
2023-05-30 19:14:31 +0200

    Hace la lógica del controller de la primera actividad a falta de los tipos

--- backend/controllers/primeraController.ts
+++ backend/controllers/primeraController.ts
@@ -1,6 +1,13 @@
 import { Request, Response } from 'express';
-
+import {IImporte} from '../interfaces/importe.interface'
+import { bruto, descuento, iva, neto } from '../helpers/funciones';
 
 export const primeraGet = async (req: Request, res: Response) => {
-  
+  const {importe} = req.params;
+  const base = parseFloat(importe);
+  const descuento: = descuento(base);
+  const bruto: = bruto(base,descuento);
+  const iva: = iva(bruto);
+  const neto: = neto(bruto,iva);
+  res.status(200).json({descuento,bruto,iva,neto});
 };
