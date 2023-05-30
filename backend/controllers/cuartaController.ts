94ae79058f604fdfde370ec690a8655022aa0be1
Bilal <mskenmasters@hotmail.com>
2023-05-30 17:34:29 +0200

    preparar los controllers segunda, tercera y cuarta

---
+++ backend/controllers/cuartaController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express';
+
+
+export const cuartaGet = async (req: Request, res: Response) => {
+  
+};
+
+export const cuartaPost = async (req: Request, res: Response) => {
+  
+};
---
+++ backend/controllers/terceraController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express';
+
+
+export const terceraGet = async (req: Request, res: Response) => {
+  
+};
+
+export const terceraPost = async (req: Request, res: Response) => {
+  
+};
