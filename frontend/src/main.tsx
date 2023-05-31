baf32819dd1f6e56b1a2e433c68964566a4df71a
Bilal <mskenmasters@hotmail.com>
2023-05-31 19:24:08 +0200

    añade cuarta en el main para renderizar

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { EjemploActividad } from './EjemploActividad';
 import 'bootstrap/dist/css/bootstrap.css';
+import { Cuarta } from './cuarta';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <EjemploActividad />
+    <Cuarta />
   </React.StrictMode>
 );
