5ed6591801677fb9c37dc7a0cf7b121ff61fa15b
piscinagua <correo_javi@hotmail.com>
2023-06-01 18:40:36 +0200

    Utiliza variables locales en lugar de tantos useStates.

--- frontend/src/cuarta.tsx
+++ frontend/src/cuarta.tsx
@@ -1,63 +1,58 @@
 import React, { useState } from "react";
 
 export const Cuarta = () => {
-    const [saldo, setSaldo] = useState<number>(0);
-    const [numeroServidor, setNumeroServidor] = useState<number>(0);
-    const [numeroCliente, setNumeroCliente] = useState<number>(0);
-    const [resultado, setResultado] = useState<string>("");
-    const [marcador, setMarcador] = useState<string>("");
-    const [empezar, setEmpezar] = useState<boolean>(false);
-    const jugarPartida = async () => {
-        setEmpezar(true);
-        setNumeroCliente(Math.floor(Math.random() * 100 + 1));
-        const response = await fetch(
-            "http://localhost:3000/api/actividades1/cuarta",
-            {
-                method: "GET",
-            }
-        );
-        const data = await response.json();
-        setNumeroServidor(data.numeroAleatorio);
+	const [saldo, setSaldo] = useState<number>(0);
+	const [resultado, setResultado] = useState<string>("");
+	const [empezar, setEmpezar] = useState<boolean>(false);
+	const jugarPartida = async () => {
+		setEmpezar(true);
+		const nCliente = Math.floor(Math.random() * 100 + 1);
+		const response = await fetch(
+			"http://localhost:3000/api/actividades1/cuarta",
+			{
+				method: "GET",
+			}
+		);
+		const data = await response.json();
+		const nServidor = data.numeroAleatorio;
 
-        if (saldo > 0) {
-            setMarcador(
-                `Marcador: cliente ${numeroCliente}, servidor ${numeroServidor}.`
-            );
-            if (numeroCliente > numeroServidor) {
-                setResultado(marcador + " ¡Has ganado!");
-                setSaldo(saldo + 10);
-            } else if (numeroCliente < numeroServidor) {
-                setResultado(marcador + " ¡Has perdido!");
-                setSaldo(saldo - 10);
-            } else {
-                setResultado(marcador + " ¡Empate!");
-            }
-        } else {
-            setResultado("Ya no puedes jugar porque se te ha acabado el saldo.");
-        }
+		if (saldo > 0) {
+			const marcador = `Marcador: cliente ${nCliente}, servidor ${nServidor}.`;
+			if (nCliente > nServidor) {
+				setResultado(marcador + " ¡Has ganado!");
+				setSaldo(saldo + 10);
+			} else if (nCliente < nServidor) {
+				setResultado(marcador + " ¡Has perdido!");
+				setSaldo(saldo - 10);
+			} else {
+				setResultado(marcador + " ¡Empate!");
+			}
+		} else {
+			setResultado("Ya no puedes jugar porque se te ha acabado el saldo.");
+		}
+	};
 
-    };
+	const handleChangeSaldo = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setSaldo(Number(e.target.value));
+	};
 
-    const handleChangeSaldo = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setSaldo(Number(e.target.value));
-    };
+	return (
+		<div>
+			<h1>Juego contra el servidor</h1>
+			<label htmlFor="saldo">Saldo</label>
+			<input
+				type="number"
+				id="saldo"
+				value={saldo}
+				onChange={handleChangeSaldo}
+				min="10"
+				step="10"
+			/>
 
-    return (
-        <div>
-            <h1>Juego contra el servidor</h1>
-            <label htmlFor="saldo">Saldo</label>
-            <input
-                type="number"
-                id="saldo"
-                value={saldo}
-                onChange={handleChangeSaldo}
-                min="10"
-                step="10"
-            />
-            <button onClick={jugarPartida}>Jugar partida contra el servidor</button>
-            <h2>Resultado</h2>
-            <p>Saldo actual: {saldo}</p>
-            {empezar && (<p>{resultado}</p>)}
-        </div>
-    );
+			<button onClick={jugarPartida}>Jugar partida contra el servidor</button>
+			<h2>Resultado</h2>
+			<p>Saldo actual: {saldo}</p>
+			{empezar && <p>{resultado}</p>}
+		</div>
+	);
 };
