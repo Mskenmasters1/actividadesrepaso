d76e5c90720de24fe14637a58772fcb89f255f16
Bilal <mskenmasters@hotmail.com>
2023-06-01 16:52:38 +0200

    cambiar cuarta.tsx con cambios de Qico

--- frontend/src/cuarta.tsx
+++ frontend/src/cuarta.tsx
@@ -1,46 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export const Cuarta = () => {
-    const [saldo, setSaldo] = useState('100');
-    const [numeroServidor, setNumeroServidor] = useState('');
-    const [resultado, setResultado] = useState('');
+    const [saldo, setSaldo] = useState<number>(0);
+    const [numeroServidor, setNumeroServidor] = useState<number>(0);
+    const [numeroCliente, setNumeroCliente] = useState<number>(0);
+    const [resultado, setResultado] = useState<string>("");
 
     const jugarPartida = async () => {
-        const numeroAleatorio = Math.floor(Math.random() * 100) + 1;
-        const response = await fetch('http://localhost:3000/api/actividades1/cuarta', {
-            method: 'GET',
-        });
+        setNumeroCliente(Math.floor(Math.random() * 100 + 1));
+        const response = await fetch(
+            "http://localhost:3000/api/actividades1/cuarta",
+            {
+                method: "GET",
+            }
+        );
         const data = await response.json();
-        const numeroServidor = data.numero;
-
-        if (numeroServidor < numeroAleatorio) {
-            setResultado('Ganaste');
-            setSaldo((prevSaldo) => (parseInt(prevSaldo) + 10).toString());
-        } else if (numeroServidor > numeroAleatorio) {
-            setResultado('Perdiste');
-            setSaldo((prevSaldo) => {
-                const newSaldo = parseInt(prevSaldo) - 10;
-                return newSaldo < 0 ? '0' : newSaldo.toString();
-            });
-        } else {
-            setResultado('Empate');
+        setNumeroServidor(data.numero);
+        if (saldo > 0) {
+            setResultado(`Marcador: cliente ${numeroCliente}, servidor ${numeroServidor}.`);
+            if (numeroCliente > numeroServidor) {
+                setResultado(resultado + ' ¡ Has ganado!');
+            }
+            else if (numeroCliente < numeroServidor) {
+                setResultado(resultado + ' ¡Has perdido!');
+            }
+            else {
+                setResultado(resultado + ' ¡Empate!');
+            }
         }
-
-        setNumeroServidor(numeroServidor.toString());
+        else {
+            setResultado('Ya no puedes jugar porque se te ha acabado el saldo.');
+        };
     };
 
     const handleChangeSaldo = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setSaldo(e.target.value);
+        setSaldo(Number(e.target.value));
     };
 
     return (
         <div>
             <h1>Juego contra el servidor</h1>
-            <p>Saldo: {saldo}</p>
-            <input type="text" value={saldo} onChange={handleChangeSaldo} />
+            <label htmlFor="saldo">Saldo</label>
+            <input
+                type="number"
+                id="saldo"
+                value={saldo}
+                onChange={handleChangeSaldo}
+                min="10"
+                step="10"
+                multiple="10"
+            />
             <button onClick={jugarPartida}>Jugar partida contra el servidor</button>
-            <p>Resultado: {resultado}</p>
-            {numeroServidor && <p>Número del servidor: {numeroServidor}</p>}
+            <h2>Resultado</h2>
+            <p>Saldo actual: {saldo}</p>
+            <p>{resultado}</p>
         </div>
     );
 };
