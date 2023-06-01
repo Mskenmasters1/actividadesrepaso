accca5ed600e6cffc0b2c898a9aef1bf2a1106ce
piscinagua <correo_javi@hotmail.com>
2023-06-01 19:29:31 +0200

    Añade el paquete de internacionalización con npm install react-intl

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,13 +6,19 @@ import { Cuarta } from "./cuarta";
 // import { Tercera } from "./Tercera";
 import { Segunda } from "./Segunda";
 import { Primera } from "./Primera";
+import { IntlProvider } from "react-intl";
+
+const locale = navigator.language;
+
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<EjemploActividad />
-		<Primera />
-		<Segunda />
-		{/* <Tercera /> */}
-		<Cuarta />
+		<IntlProvider locale={locale} >
+			<EjemploActividad />
+			<Primera />
+			<Segunda />
+			{/* <Tercera /> */}
+			<Cuarta />
+		</IntlProvider>
 	</React.StrictMode>
 );
