157ef7e6eb3a0064484c3ff39617e12e00c0f531
Quico Saval <quicosv@gmail.com>
2023-06-02 17:56:09 +0200

    Descomenta la línea de la tercera actividad en el main para que se pueda visualizar

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -17,7 +17,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 			<EjemploActividad />
 			<Primera />
 			<Segunda />
-			{/* <Tercera /> */}
+			<Tercera />
 			<Cuarta />
 		</IntlProvider>
 	</React.StrictMode>
